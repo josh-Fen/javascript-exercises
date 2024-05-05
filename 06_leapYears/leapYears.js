3d156f6c0fb0e8fa8e2c4c289ad95a6359ba0122
josh <8550998+josh-Fen@users.noreply.github.com>
2024-05-05 14:24:46 -0400

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    let answer = false;
+    if (year % 4 === 0) {
+        answer = true;
+        if (year % 100 === 0 && year % 400 !== 0) {
+            answer = false;
+        }
+    }
+    return answer
 };
 
 // Do not edit below this line
