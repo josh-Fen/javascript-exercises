b0c79e8c8ef552c01b419a4dc5643cb43cab919b
josh <8550998+josh-Fen@users.noreply.github.com>
2024-05-05 01:01:19 -0400

    Complete exercises 4 and 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+    arr = arr.filter((item) => !args.includes(item));
 
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+    if (typeof (first) === 'number' &&
+        typeof (second) === 'number' &&
+        first > 0 &&
+        second > 0) {
+        const start = first < second ? first : second;
+        const end = second > first ? second : first;
+        let answer = 0;
+        for (let i = start; i <= end; i++) {
+            answer += i;
+        }
+        return answer;
+    } else {
+        return "ERROR";
+    }
 
 };
 
