4d31154fb0b5c46ca47310224de6c74e92391714
josh <8550998+josh-Fen@users.noreply.github.com>
2024-05-04 23:33:21 -0400

    Complete exercises 1 through 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+    let answer = '';
+    for (let i = 0; i < num; i++) {
+        answer += str;
+    }
+    if (num < 0) {
+        answer = 'ERROR';
+    }
+    return answer;
 
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    const forward = [];
+    const reversed = [];
+    for (let i = 0; i < str.length; i++) {
+        forward.push(str.charAt(i));
+    }
+    for (let i = forward.length - 1; i >= 0; i--) {
+        reversed.push(forward[i]);
+    }
+    return reversed.join('');
 };
 
 // Do not edit below this line
