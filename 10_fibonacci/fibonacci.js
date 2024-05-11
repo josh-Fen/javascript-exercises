88358167f471f1ec761b7fd75b0a11e823c2fdf9
josh <8550998+josh-Fen@users.noreply.github.com>
2024-05-11 19:40:09 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    const n = +num;
+    if (n < 0) {
+        return 'OOPS';
+    } else if (n === 0) {
+        return 0;
+    } else {
+        arr = [1, 1];
+        for (let i = 3; i <= n; i++) {
+            const num1 = arr[i - 3];
+            const num2 = arr[i - 2];
+            arr.push(num1 + num2);
+        }
+        return arr[n - 1];
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
