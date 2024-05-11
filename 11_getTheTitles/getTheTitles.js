5309650a3d9ccdc440e292a58ac850de68c7be8c
josh <8550998+josh-Fen@users.noreply.github.com>
2024-05-11 19:45:52 -0400

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (arr) {
+    // const titles = [];
+    // arr.forEach(element => {
+    //     titles.push(element.title);
+    // });
+    // return titles;
 
+    return arr.map(element => element.title);
 };
 
 // Do not edit below this line
