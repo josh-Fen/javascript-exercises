21de113d6e94f32df72eea9c0bf1c4ed3f0c6834
josh <8550998+josh-Fen@users.noreply.github.com>
2024-05-11 20:13:07 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    return arr.reduce((obj, element) => {
 
+        if (!obj.yearOfBirth) {
+            return element;
+        }
+
+        const birth = element.yearOfBirth;
+        const death = element.yearOfDeath ? element.yearOfDeath : new Date().getFullYear();
+        const yearsAlive = death - birth;
+
+        const objBirth = obj.yearOfBirth;
+        const objDeath = obj.yearOfDeath ? obj.yearOfDeath : new Date().getFullYear();
+        const objYearsAlive = objDeath - objBirth;
+
+        return yearsAlive > objYearsAlive ? element : obj;
+    }, {});
 };
 
 // Do not edit below this line
